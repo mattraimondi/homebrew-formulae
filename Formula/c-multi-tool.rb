77539438edef5d08cf09a9cffcf8077e5274e67d
Matthew <huggyraimondi@gmail.com>
2019-01-13 14:45:38 -0500

    update cmult

--- Formula/c-multi-tool.rb
+++ Formula/c-multi-tool.rb
@@ -2,12 +2,12 @@ class CMultiTool < Formula
   homepage 'https://github.com/mattraimondi/c-multi-tool'
   url 'https://github.com/mattraimondi/c-multi-tool/archive/v1.0.0.zip'
   version "1.0.0"
-  sha256 'eea95ae35b6329c13cabda69c214afe0b33d1853d1d1fedd1ece8f3df2112d88'
+  sha256 'bd894cbb54db0f89af766ccf019adcba8e573a176b73a4b149caab87cb3378ea'
 
   bottle :unneeded
 
   def install
-    system "make", "compile"
-    system "make", "install"
+    system "cc", "c-multi-tool.c", "-o", "c-multi-tool"
+    bin.install "c-multi-tool"
   end
 end
