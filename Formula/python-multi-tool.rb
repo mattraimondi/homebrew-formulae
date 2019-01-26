846c45845a36837045df2ea2f91e9290d60addc4
Matthew <huggyraimondi@gmail.com>
2019-01-26 15:26:57 -0500

    update python-multi-tool

--- Formula/python-multi-tool.rb
+++ Formula/python-multi-tool.rb
@@ -1,8 +1,8 @@
 class PythonMultiTool < Formula
   homepage 'https://github.com/mattraimondi/python-multi-tool'
-  url 'https://github.com/mattraimondi/python-multi-tool/archive/v1.0.3.zip'
-  version "1.0.3"
-  sha256 '608807eb949a8fd42c68f9be1253d98f8696e75cc16ef2782a3b3be8f058b572'
+  url 'https://github.com/mattraimondi/python-multi-tool/archive/v1.1.0.zip'
+  version "1.1.0"
+  sha256 '08083a28222ecaee3f09a7635a6d2e9eda786f0a7cdcac093e7e11a1de868377'
 
   depends_on "python3"
 
